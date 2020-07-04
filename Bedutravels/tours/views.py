561362e71030f2c30a0c331f71b7b940d61a6b9b
Maricela Sanchez <mayela0x14@gmail.com>
2020-07-04 11:38:12 -0500

    Change view. Add styles

--- Bedutravels/tours/views.py
+++ Bedutravels/tours/views.py
@@ -1,7 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
 def index(request):
     """ Vista para atender la petición de la url / """
-    return HttpResponse("<h2>Soy la página de inicio!</h2>")
+    return render(request, "tours/index.html")
