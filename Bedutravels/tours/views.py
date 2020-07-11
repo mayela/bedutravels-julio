50f0e64b3c8bb06b0326c4bf1ba3d2e7523a80dd
Maricela Sanchez <mayela0x14@gmail.com>
2020-07-11 11:28:05 -0500

    Reuse user auth. Add auth views

--- Bedutravels/tours/views.py
+++ Bedutravels/tours/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+from .models import Tour
+
+@login_required
 def index(request):
     """ Vista para atender la petición de la url / """
-    return render(request, "tours/index.html")
+    # Obteniendo los datos mediantes consultas
+    tours = Tour.objects.all()
+
+    return render(request, "tours/index.html", {"tours":tours})
